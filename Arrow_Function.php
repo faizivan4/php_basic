7c16c1f4a767fc121a0f3687c6b1e31b1a284727
faizivan4 <faizivan4@gmail.com>
2022-07-20 21:36:06 +0700

    add arrow function

---
+++ Arrow_Function.php
@@ -0,0 +1,18 @@
+<?php
+//Arrow Function
+$firstName = "Faiz";
+$lastName = "Dharmawan";
+
+// $arrowFunction = fn() => "Hallo $firstName"."\n";
+
+echo $arrowFunction();
+
+/*
+Arrow Function :
+    - ARROW FUNCTION HANYA BISA DI GUNAKAN DI PHP 7.4
+    - Arrow Function adalah sebagai ALTERNATIF Anonym Function yang lebih sederhana dalam pembuatannya
+    - Arrow Function dan Anonymous Function adalah dua hal yang sama
+    - Yang MEMBEDAKAN Arrow Function adalah, secara otomatis variable di luar Closure bisa digunakan, tidak seperti Anonym Function yang harus di sebut menggunakan use(namaVariable)
+    - Pembuatan Arrow Function tidak menggunakan kata Function, tapi FN
+    - Arrow Function hanya di gunakan pada pembuatan function yang sangat SEDERHANA.
+*/
