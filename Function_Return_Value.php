abcc74eeb098d96f6f5a4117fc96b16d5e0422e3
faizivan4 <faizivan4@gmail.com>
2022-07-20 16:50:52 +0700

    edit pada Function Return Value.php

--- Function_Return_Value.php
+++ Function_Return_Value.php
@@ -30,12 +30,34 @@ function getFinalValue(int $value)
 }
 
 $score = getFinalValue(10);
-echo "Nilai anda adalah : $score";
+echo "Nilai anda adalah : $score"."\n";
+
+// Return Type Declaration
+function namaSiswa (string $firstName, string $middName):string //:string digunakan untuk menjaga agar tidak mengembalikan/Return tipe data yang salah.
+{
+    $fullName = $firstName.$middName;
+    return $fullName;
+}
+
+$fullName = namaSiswa("Faiz ","Ivan");
+echo "nama anda adalah : $fullName";
 
 /*
+Function Return Value 
+
 NOTE :
 - Secara Default, Function tidak menghasilkan value apapun, namun jika dibutuhkan, kita bisa membuat Function yang mengembalikan value/nilai
 - Di dalam blok Function, untuk menghasilkan nilai/value tersebut, kita bisa menggunakan keyword RETURN. lalu di ikuti dengan data yang ingin di hasilkan
 - Kita hanya bisa menghasilkan 1 data di sebuah Function, tidak bisa lebih dari satu.
+
+Return Type Declaration 
+Note :
+
+- Sama seperti paramater, Pada Function Return juga bisa di deklarasikan tipe data nya
+- Berfungsi untuk memudahkan ketika membaca tipe data kembalian Function, bisa juga untuk menjaga agar tidak mengembalikan tipe data yang salah pada function
+- Struktur Syntax :
+    
+    function namaFunction(int namaParameter): int
+
 
 */
