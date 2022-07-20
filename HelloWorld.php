a25a1b7d7604117372a18d458865247eb03b6797
faizivan4 <faizivan4@gmail.com>
2022-07-20 11:39:29 +0700

    add file HelloWorld.php

---
+++ HelloWorld.php
@@ -0,0 +1,11 @@
+<?php
+echo"Hello World";
+
+/*
+
+NOTE :
+- Struktur penulisan syntax PHP adalah diawali dengan tag <?php (sebagai tag pembuka), dan di tutup dengan tag ?> 
+(tag penutup ?> sifatnya tidak wajib)
+- tag ?> digunakan biasanya ketika syntax php di gabung dengan syntax HTML
+- "echo" di gunakan untuk menampilkan/print out tulisan
+*/
